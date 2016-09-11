7f261d00da8857fae8a37a7a602b1589f2261790
wasif <wasif@hotmail.com>
2016-09-12 00:06:18 +0500

    Assignment completed

---
+++ assignment#1,2,3/app.js
@@ -0,0 +1,16 @@
+/////// ass 1///////////////////
+//// alert an input data//////
+var input= prompt("Please write something","wirte here"); 
+alert(input);
+///////// ass 2//////////////
+//////// take two num from user n show their sum////////////
+var num1=prompt("Enter Num 1");
+var num2=prompt("Enter Num 2");
+num1 = parseInt(num1);
+num2 = parseInt(num2);
+alert(num1+num2);
+////////// ass3///////////////
+//// conatenate two names//////////
+var name1=prompt("enter first name");
+var name2=prompt("enter last name");
+alert(name1+name2);
